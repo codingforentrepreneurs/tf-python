71d70db0f2060fd0f0b8cc4596feecb1d80db4b8
Coding For Entrepreneurs <codingforentrepreneurs@gmail.com>
2023-04-25 12:58:54 -0500

    Create test_gunicorn.py

---
+++ src/test_gunicorn.py
@@ -0,0 +1,24 @@
+import os
+import subprocess
+import time
+
+import requests
+
+def test_gunicorn_start():
+    gunicorn_process = subprocess.Popen(
+        ["gunicorn", "-w", "1", "main:app", "-b", "127.0.0.1:8000"]
+    )
+    time.sleep(2)  # Give Gunicorn some time to start
+
+    try:
+        response = requests.get("http://127.0.0.1:8000")
+        assert response.status_code == 200
+        assert response.json() == {
+            "hello": "world",
+            "cron": "smooth-cronjob",
+            "watchtower": "working",
+            "env-message": os.environ.get("ENV_MESSAGE") or "Nothing to report",
+        }
+    finally:
+        gunicorn_process.terminate()
+        gunicorn_process.wait()
