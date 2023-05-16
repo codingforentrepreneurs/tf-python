6942825fac0e6dc310dcb5e64de91e79edc8bfcf
CodingForEntrepreneurs <hello@teamcfe.com>
2023-05-16 11:31:45 -0500

    Added another environment variable

--- src/main.py
+++ src/main.py
@@ -7,6 +7,10 @@ def get_env_message():
     return os.environ.get("ENV_MESSAGE") or "Nothing to report"
 
 
+def get_secret_message():
+    return os.environ.get("SECRET_MESSAGE") or "Nothing lurking"
+
+
 @app.get("/")
 def home_view():
-    return  {"hello": "world", "cron": "smooth-cronjob", "watchtower": "working", "env-message": get_env_message()}
+    return  {"hello": "world", "cron": "smooth-cronjob", "watchtower": "working", "env-message": get_env_message(), "secret-message": get_secret_message()}
